56d7ad96a54b816edf88167c229cbdbdc260eb33
akashilanthendral-tringapps <akash.ilanthendral@tringapps.com>
2021-12-02 12:10:01 +0530

    added greatestProduct.java file

---
+++ greatestProduct.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class greatestProduct
+{
+    public static void reverse(int[] array)
+    {
+  
+        int n = array.length;
+        for (int i = 0; i < n / 2; i++) {
+            int temp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = temp;
+        }
+    }
+    
+    
+	public static void main(String[] args) {
+	    System.out.println("enter the number of elements: ");
+	    Scanner s = new Scanner(System.in);
+	    final int size = s.nextInt();
+	    //if the number of elements is less than three, then greatest of the product of "three" numbers cant be performed.
+	    if(size < 3){
+	        System.out.println("number of elements should be greater than or equal to three!");
+	    }
+		int[] arr =  new int[size];
+		
+		for(int i = 0; i < size; i++){
+		    arr[i] = s.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		reverse(arr);
+		
+		
+		for(int i = 0; i < size; i++){
+		    System.out.println(arr[i] + " ");
+		}
+		
+		System.out.println(arr[0] * arr[1] * arr[2]);
+	}
+}
